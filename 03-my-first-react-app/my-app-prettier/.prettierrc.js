24e271d2861b9bccce76bad906a18fceea26cc20
Beren <berenickt99@gmail.com>
2023-07-24 23:40:52 +0900

    FIX:03-prettier-package-script-설정

--- 03-my-first-react-app/my-app-prettier/.prettierrc.js
+++ 03-my-first-react-app/my-app-prettier/.prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 100,
+  singleQuote: true, // 싱글쿼트를 사용
+  trailingComma: 'all', // 콤파(,)를 추가할 수 있다면, 콤마를 추가
+  printWidth: 100, // 한 줄에 작성할 수 있는 최대 코드 문자 수를 설정
 };
