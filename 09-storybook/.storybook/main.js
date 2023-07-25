1418be98034bab1da60d060118d0aa89f2505ce6
Beren <berenickt99@gmail.com>
2023-07-25 23:56:16 +0900

    FIX:스토리북-세팅

--- 09-storybook/.storybook/main.js
+++ 09-storybook/.storybook/main.js
@@ -1,16 +1,17 @@
+// **** 스토리북 메인 설정 파일
+// 스토리북은 컴포넌트 표시를 위해 자체 개발 서버를 사용하는데,
+// main.js은 해당 서버에 관한 설정 파일임
+// 스토리북을 실행하는데 필수설정 팡리이라, 수정하는 경우 스토리북을 재실행해야 함
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
--- 09-storybook/.storybook/preview.js
+++ 09-storybook/.storybook/preview.js
@@ -1,3 +1,6 @@
+// **** 컴포넌트를 어떻게 표시할지 관리하는 설정 파일
+// 스토리북에서 사용한느 전역 매개변수, 전역코드, 데코레이터를 관리
+// 스토리북의 특정 설정을 변경해야 한다면, .storybook 폴더 안에서 내용을 수정
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
