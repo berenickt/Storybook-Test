f80023b65d6321df8c06c18cde3a378730a07980
Beren <berenickt99@gmail.com>
2023-07-26 23:12:43 +0900

    ADD:주석-설명-예시

--- 10-cdd-counter/src/components/atoms/Button/index.tsx
+++ 10-cdd-counter/src/components/atoms/Button/index.tsx
@@ -1,7 +1,8 @@
-import styled from '@emotion/styled';
+import styled from '@emotion/styled'
 
 const Container = styled.button`
   border: 0;
+  //
   color: #ffffff;
   background-color: #ff5722;
   cursor: pointer;
@@ -16,13 +17,13 @@ const Container = styled.button`
   &:active {
     box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
   }
-`;
+`
 
 interface Props {
-  readonly label: string;
-  readonly onClick?: () => void;
+  readonly label: string
+  readonly onClick?: () => void
 }
 
 export const Button = ({ label, onClick }: Props) => {
-  return <Container onClick={onClick}>{label}</Container>;
-};
+  return <Container onClick={onClick}>{label}</Container>
+}
