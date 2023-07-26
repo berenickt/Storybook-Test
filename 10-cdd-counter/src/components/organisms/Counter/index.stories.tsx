825d714d4e673d2a7d9881cf0fa5c6b6d78455df
Beren <berenickt99@gmail.com>
2023-07-27 00:02:08 +0900

    FIX:유기체-Counter

--- 10-cdd-counter/src/components/organisms/Counter/index.stories.tsx
+++ 10-cdd-counter/src/components/organisms/Counter/index.stories.tsx
@@ -1,12 +1,16 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+// 1. 스토리를 작성하기 위해 기본적으로 import
+import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-import { Counter } from '.';
+import { Counter } from '.'
 
+// 2. Organisms라는 메뉴 하위에 Counter이라고 표시
+// 3. 컴포넌트는 Counter
 export default {
   title: 'Organisms/Counter',
   component: Counter,
-} as ComponentMeta<typeof Counter>;
+} as ComponentMeta<typeof Counter>
 
-const Template: ComponentStory<typeof Counter> = () => <Counter />;
+// 4. 하나의 스토리 파일에 여러 스토리를 작성하도록 <Counter /> 컴포넌트 템플릿을 선언
+const Template: ComponentStory<typeof Counter> = () => <Counter />
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
