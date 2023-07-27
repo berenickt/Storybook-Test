9e55f2a4af5f0c3ae3ffc46dbadfba77f7196a95
Beren <berenickt99@gmail.com>
2023-07-27 22:23:14 +0900

    ADD:Label-원자-컴포넌트

--- 11-cdd-todo/src/components/atoms/Label/index.stories.tsx
+++ 11-cdd-todo/src/components/atoms/Label/index.stories.tsx
@@ -1,15 +1,21 @@
+// 1. 스토리를 작성하기 위해 기본적으로 import
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Label } from '.';
 
+// 2. ATOMS라는 메뉴 하위에 Label이라고 표시
+// 3. 컴포넌트는 Label
 export default {
   title: 'Atoms/Label',
   component: Label,
 } as ComponentMeta<typeof Label>;
 
+// 4. 하나의 스토리 파일에 여러 스토리를 작성하도록 <Label /> 컴포넌트 템플릿을 선언
+// Label 컴포넌트에 args 객체를 전달하여 동적인 컴포넌트를 생성
 const Template: ComponentStory<typeof Label> = (args) => <Label {...args} />;
 
 export const Default = Template.bind({});
+// Label 컴포넌트의 스토리인 Default의 필수 Props인 label 설정
 Default.args = {
   label: '리액트 공부하기',
 };
