160bdd63d794c409c596b448a68cff5ea9d503f0
Beren <berenickt99@gmail.com>
2023-07-28 00:09:39 +0900

    ADD:11-TodoListPage-페이지-컴포넌트

--- 11-cdd-todo/src/pages/ToDoInputPage/index.stories.tsx
+++ 11-cdd-todo/src/pages/ToDoInputPage/index.stories.tsx
@@ -1,12 +1,16 @@
+// 1. 스토리를 작성하기 위해 기본적으로 import
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { ToDoInputPage } from '.';
 
+// 2. Pages라는 메뉴 하위에 ToDoInputPage이라고 표시
+// 3. 컴포넌트는 ToDoInputPage
 export default {
   title: 'Pages/ToDoInputPage',
   component: ToDoInputPage,
 } as ComponentMeta<typeof ToDoInputPage>;
 
+// 4. 하나의 스토리 파일에 여러 스토리를 작성하도록 <ToDoInputPage /> 컴포넌트 템플릿을 선언
 const Template: ComponentStory<typeof ToDoInputPage> = () => <ToDoInputPage />;
 
 export const Default = Template.bind({});
