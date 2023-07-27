594b2a13004bfb1d0e56bdd724a20b74daf2849f
Beren <berenickt99@gmail.com>
2023-07-27 19:19:51 +0900

    ADD:AppTitle-원자-컴포넌트

--- 11-cdd-todo/src/components/atoms/AppTitle/index.stories.tsx
+++ 11-cdd-todo/src/components/atoms/AppTitle/index.stories.tsx
@@ -1,10 +1,14 @@
+// 1. 스토리를 작성하기 위해 기본적으로 import
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { AppTitle } from '.';
 
+// 2. ATOMS라는 메뉴 하위에 AppTitle이라고 표시
+// 3. 컴포넌트는 AppTitle
 export default {
   title: 'Atoms/AppTitle',
   component: AppTitle,
+  // background 옵션으로 원하는 색상 추가
   parameters: {
     backgrounds: {
       default: 'Header background color',
@@ -13,6 +17,7 @@ export default {
   },
 } as ComponentMeta<typeof AppTitle>;
 
+// 4. 하나의 스토리 파일에 여러 스토리를 작성하도록 <AppTitle /> 컴포넌트 템플릿을 선언
 const Template: ComponentStory<typeof AppTitle> = () => <AppTitle />;
 
 export const Default = Template.bind({});
